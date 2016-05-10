c2b555d2ca640005fd62d99af21f5b5ba2cda582
Andrew Tan <andrewvmail@gmail.com>
2016-05-10 00:21:18 -0700

    Move site and Claude changes

---
+++ cache/template/yoo_lava.php
@@ -0,0 +1,6 @@
+{
+    "tmpl_check": "894e318e387edb10693d65cc553c35b5",
+    "tmpl_data": "{\"status\":\"error\",\"message\":\"Sorry dude, this application is unknown.\"}",
+    "warp_check": "5dea4386755eaa3a6ac24623704727bd",
+    "warp_data": "{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\" target=\\\"_blank\\\">Download Warp Theme Framework 7.3.1<\\\/a>\"}"
+}
---
+++ cache/widgetkit/widgetkit.php
@@ -0,0 +1,1 @@
+{"update_check":"d5f38dfacabc6baf6d1a1d1d58fb795d","update_data":"{\"status\":\"update-available\",\"message\":\"New version available! <a href=\\\"http:\\\/\\\/www.yootheme.com\\\/widgetkit\\\/downloads\\\" target=\\\"_blank\\\">Download Widgetkit 1.5.5<\\\/a>\"}"}
